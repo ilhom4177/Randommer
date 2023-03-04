386f946f8dc6044143347fde3023823488f96860
ilhom <ilhommamatqulov276@gmail.com>
2023-03-05 00:04:46 +0500

    ✋

--- name.py
+++ name.py
@@ -14,7 +14,12 @@ class Name(Randommer):
         Returns:
             list: list of names
         '''
-        pass
+        headers={ 'X-Api-Key':api_key}
+        poyloud={  'nameType': nameType,
+                    'quantity': quantity,
+                }
+        r=requests.get(f'{self.get_url()}Name', params=poyloud , headers=headers )
+        return r.json()
     
     def get_name_suggestions(self, api_key: str, startingWords: str) -> list:
         '''get name suggestions
@@ -26,7 +31,10 @@ class Name(Randommer):
         Returns:
             list: list of name suggestions
         '''
-        pass
+        headers={ 'X-Api-Key': api_key}
+        params={'startingWords' : startingWords }
+        r=requests.get(f'{self.get_url()}Name/Suggestions',headers=headers,params=params)
+        return r.json()
     
     def get_name_cultures(self, api_key: str) -> list:
         '''get available cultures
@@ -37,4 +45,15 @@ class Name(Randommer):
         Returns:
             list: list of names
         '''
-        pass
+        headers={ 'X-Api-Key': api_key}
+        r=requests.get(f'{self.get_url()}Name/Cultures',headers=headers)
+        return r.json()
+
+
+
+
+ans=Name()
+key = "940a688e878544858234dee258149563"
+print(ans.get_name(key,'fullname',5))
+print(ans.get_name_suggestions(key,'hello'))
+print(ans.get_name_cultures(key))
