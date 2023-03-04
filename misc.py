fd98b904946a9f530d36489a6299efe01a623565
ilhom <ilhommamatqulov276@gmail.com>
2023-03-04 23:57:42 +0500

    ✋

--- misc.py
+++ misc.py
@@ -12,7 +12,9 @@ class Misc(Randommer):
         Returns:
             list: list of cultures
         '''
-        pass
+        headears={ 'X-Api-key': api_key}
+        r=requests.get(f'{self.get_url()}Misc/Cultures',headers=headears)
+        return r.json()
     
     def get_random_address(self, api_key: str, number: int, culture='en') -> list:
         '''get available misc cultures
@@ -25,4 +27,15 @@ class Misc(Randommer):
         Returns:
             list: random address
         '''
-        pass
+        headers={ 'X-Api-Key': api_key }
+        poylaud={ 
+            'number': number,
+            'culture': culture
+                }
+        r=requests.get(f'{self.get_url()}Misc/Random-Address', params=poylaud, headers=headers)
+        return r.json()
+
+ans=Misc()
+key = "940a688e878544858234dee258149563"
+print(ans.get_random_address(api_key=key, number=5))
+print(ans.get_cultures(key))
